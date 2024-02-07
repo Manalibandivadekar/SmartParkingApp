f8c287928091a0682d2555fc077c15fac5700231
Chiranth Krishna K <chiranthkrishnak@gmail.com>
2024-02-07 22:33:16 +0530

    works now

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 // main.dart
-import 'package:flutter/material.dart';
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:parking_app/main_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  
+    await Firebase.initializeApp(options: FirebaseOptions(
+    apiKey: 'AIzaSyB4Zd9pxcb_FGDb8czLW-UVSB6n0flPcTc', 
+    appId: '1:9694087846:android:0db5cbf76ee5663daec267', 
+    messagingSenderId: '9694087846', 
+    projectId: 'smart-parking-app-30420'));
+    
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +27,7 @@ class MyApp extends StatelessWidget {
       title: 'Smart Parking System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
+      ),      
       home: const MainPage(),
     );
   }
